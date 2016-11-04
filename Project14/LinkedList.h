d63e6b6ebb5dd7d673bd3183368806aa5ff787b3
Conner-Stack <connerstack@gmail.com>
2016-11-04 17:54:06 -0500

    LinkedList almost complete push

--- Project14/LinkedList.h
+++ Project14/LinkedList.h
@@ -9,11 +9,11 @@ using namespace std;
 template < typename T>
 struct nodeType
 {
-	int info;
-	nodeType * link;
+	int Info;
+	nodeType * Link;
 };
 template <typename T>
-class linkedLIstIterator
+class linkedListIterator
 {
 private:
 	//pointer to point to the current node in the linked list
@@ -28,22 +28,22 @@ public:
 	linkedListIterator(nodeType<T> *node)
 	{
 		current = node;
-	};
+	}
 
 	//Function to overload the dereferencing operator *
 	//Postcondition:Returns the info contained in the node
 	T operator*()
 	{
-		return current->info;
-	};
+		return current->Info;
+	}
 
 	//Overload the pre-increment operator
 	//Postcondition: The iterator is advanced to the next node
 	linkedListIterator<T> operator++()
 	{
-		linkedListIterator<T> *addit;
-		current = addit++;
-	};
+		this->current = current->Link;
+		return *this
+	}
 
 	//Overlaod the equality operator
 	//Postcondition: Returns true if this iterator is equal to the 
@@ -56,7 +56,7 @@ public:
 		}
 		else
 			return false;
-	};
+	}
 
 	//Overlaod the not equal operator
 	//Postcondition: Returns true if this iterator is not equal to the 
@@ -69,106 +69,240 @@ public:
 		}
 		else
 			return false;
-	};
+	}
 };
 
 template<typename T>
 class linkedListType
 {
 protected:
-	int count; //variable to store the number of elements in the list
-	nodeType<T> *first; //pointer to the first node of the list
-	nodeType<T> *last; //pointer to the last node of the list
+	int Count; //variable to store the number of elements in the list
+	nodeType<T> *First; //pointer to the first node of the list
+	nodeType<T> *Last; //pointer to the last node of the list
 public:
 	//Overload the assignment operator
-	const linkedListType<T>& operator= (const linkedListType<T>& otherList);
+	const linkedListType<T>& operator= (const linkedListType<T>& otherList)
+	{
+		this->First = otherList->first;
+		Last = First;
+		if (Last->Link != NULL)
+			this->Last->Link = otherList->last->link;		
+		this->Last = otherList->last;
+	}
 	
 	//Initialize the list to an empty state
 	//Postcondition: first = NULL, last = NULL, count = 0;
 	void initializeList()
 	{
-		first = NULL;
-		last = NULL:
+		First = NULL;
+		Last = NULL:
 		count = 0;
-	};
+	}
 
 	//Function to determine whether the list is empty
 	//Postcondition: Returns true if the list is empty otherwise it returns false
 	bool isEmptyList() const
 	{
-		if (first == NULL)
+		if (First == NULL)
 		{
 			return true;
 		}
 		else
 			return false;
-	};
+	}
 
 	//Function to output the data contained in each node
 	//Postcondition: Node
 	void print() const
 	{
 		
+		nodeType<T> * ptr = First;
+		while (ptr != NULL)
+		{
+			cout << ptr << endl;
+			ptr = ptr->Link;
+		}
+		
 	
-	};
+	
+	}
 
 	//Function to return the number of nodes in the list
 	//Postcondition: The value of count is returned
-	int length() const;
+	int length() const
+	{
+		return Count;
+	}
 
 	//Function to delete all the nodes from the list
 	//Postcondition: first = NULL, last = NULL, count = 0;
-	void destroyList();
+	void destroyList() 
+	{
+		nodeType<T> * listdest;
+		listdest = First;
+		
+		while (listdest != NULL)
+		{
+			First = First->Link;
+			delete listdest;
+			listdest->First;
+			count--;
+		}
+		First = NULL;
+			Last = NULL;
+			count = 0;
+	}
 
 	//Function to return the first element in the list
 	//Precondition: The list must exist and must not be empty
 	//Postcondition: If the list is empty, the program terminates; otherwise,
 	//the first element of the list is returned
-	T front() const;
+	T front() const
+	{
+		if (this->First = NULL)
+		{
+			return NULL;
+	}
+		else
+		{
+			return *this->First;
+		}
+	
+	}
 
 	//Function to return the last element in the list
 	//Precondition: The list must exist and must not be empty
 	//Postcondition: If the list is empty, the program terminates; otherwise,
 	//the last element of the list is returned
-	T back() const;
+	T back() const
+	{
+		if (this->Last = NULL)
+		{
+			return NULL;
+		}
+		else
+			return *this->Last;
+	
+	
+	}
+		
 
 	//Function to determine whether node is in the list
 	//Postcondition: Returns true if node is in the list
-	//otherwise teh value false is returned
-	bool search(const T& nodeInfo);
+	//otherwise the value false is returned
+	bool search(const T& nodeInfo)
+	{
+		nodeType<T> *ptr = First;
+		while (ptr != NULL)
+		{
+			if (ptr->Info == nodeInfo)
+			{
+				return true;
+			}
+			ptr = ptr->Link;
+		}
+		return false;
+	}
 
 	//Function to insert node at the begining of the list
 	//Postcondition: first points to the new list, node is inserted 
 	//at the beginning of the list, last points to the last node in
 	//the list, and count is incremented by 1;
-	void insertFirst(const T& nodeInfo);
+	void insertFirst(const T& nodeInfo)
+	{
+		nodeType<T> *Node = new nodeType<T>;
+		Node->info = nodeInfo;
+		Node->Link = First;
+		if (First == NULL)
+		{
+			First = node;
+			Count++
+		}
+		}
 
 	//Function to insert node at the end of the list
 	//Postcondition: first points to the new list, node is inserted 
 	//at the beginning of the list, last points to the last node in
 	//the list, and count is incremented by 1;
-	void insertLast(const T& nodeInfo);
+	void insertLast(const T& nodeInfo)
+	{
+		nodeType<T> *Node = new nodeType<T>;
+			Node->Info = nodeInfo;
+			Node->Link = NULL;
+		if (First == NULL)
+			this->insertFirst(nodeInfo)
+		else
+		{
+			Last->Link = Node;
+			Last = Node;
+			Count++;
+		}
+	}
 
 	//Function to delete node from the list
 	//Postcondition: If found, the node containing the node is deleted from the list. first points to
 	//the first node, last points to the last node of the update list, and count is decremented by 1
-	void deleteNode(const T& nodeInfo);
+	void deleteNode(const T& nodeInfo)
+	{
+		nodeType<T> *ptr = First;
+		nodeType<T> *tmp = First;
+		while (ptr != NULL)
+		{
+			
+			if (ptr->Link->Info = nodeInfo)
+			{
+				tmp = ptr->Link;
+				ptr->Link = ptr->Link->Link;
+					delete tmp;
+				Count--;
+			}
+			if (ptr->Link = NULL)
+			{
+				Last = ptr;
+				break;
+			}
+			if (ptr->Info = nodeInfo)
+			{
+				tmp = ptr;
+				ptr = ptr->Link;
+				delete tmp;
+				Count--;
+				//first = ptr;
+			}
+	ptr = ptr->Link;	
+		}
+
+	}
 
 	//Function to return an iterator at the begining of the linked list
 	//Postcondition: Returns an iteratir such that the current is set to first
-	linkedListIterator<T> begin();
+	linkedListIterator<T> begin()
+	{
+		return linkedListIterator<T>(First);
+	}
 
 	//Funcion to rturn an iterator at the end of the linked list
 	//Postcondition: Returns an iterator such that current is set to NULL
-	linkedListIterator<T> end();
+	linkedListIterator<T> end()
+	{
+		return linkedListIterator(Last);
+	};
 
 	//Default constructor
 	//Initializes the list to an empty state 
 	//Postcondition: first = NULL, last = NULL, count = 0;
-	linkedListType();
+	linkedListType()
+	{
+		First = NULL;
+		Last = NULL;
+		Count = 0;
+	}
 
 	//copy constructor
-	linkedListType(const linkedListType<T> otherList);
+	linkedListType(const linkedListType<T> otherList)
+	{
+	
+	}
 
 	//deconstructor
 	//Deletes all the nodes from the list
@@ -178,11 +312,22 @@ public:
 private:
 	//Function to make a copy of list
 	//Postcondition: A copy of list is created and assigned to this list
-	void copyList(const linkedListType<T>& otherList);
+	void copyList(const linkedListType<T>& otherList)
+	{
+		nodeType<T> * Node;
+		Node = otherList.First;
+		this->destroyList();
+		while (Node != NULL)
+		{
+			this->insertFirst(Node->Info);
+			this->insertLast(Node->Info);
 
 
 
 
 
+			Node = Node->Link;
+		}
+		}
 
 };
--- Project14/Source.cpp
+++ Project14/Source.cpp
@@ -14,15 +14,20 @@ struct nodeType
 
 int main()
 {
-	nodeType * Head;
-	nodeType a, b, c;
-	c.info = 4;
-	b.info = 2;
-	a.info = 0;
-	c.link = &b;
-	b.link = &a;
-	Head = &c;
-	cout << Head->info << c.link->info << b.link->info;
-	system("pause");
+	//nodeType * Head;
+	//nodeType a, b, c;
+	//c.info = 4;
+	//b.info = 2;
+	//a.info = 0;
+	//c.link = &b;
+	//b.link = &a;
+	//Head = &c;
+	//cout << Head->info << c.link->info 
+
+
+
+
+
+	system("pause");<< b.link->info;
 	return 0;
 }
